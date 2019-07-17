82a8ce3c1ebf33cfcd6952ede0d49adb619c83c9
Piyush Jain <piyushjain964349@gmail.com>
2019-07-17 20:45:03 +0530

    Done with Api fetching and builded a basic UI Application uses built-in android location api for location services To be Improved -> 	Need to add Google playservices location api 	Improve UI and other settings

--- app/src/main/java/com/example/mausam/MainActivity.java
+++ app/src/main/java/com/example/mausam/MainActivity.java
@@ -1,29 +1,74 @@
 package com.example.mausam;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.location.Location;
+
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.json.JSONStringer;
-
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 
 //http://api.openweathermap.org/data/2.5/weather?q=mumbai&appid=938bb3a45ec1e77e05c50788aeaaf94f
 
 public class MainActivity extends AppCompatActivity {
 
-    String curr_weather = "";
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    // For Debug
+    String placeName;
+    String []showDetails = {"Invalid","Invalid","-","-","-","-","-","-","-"};
+
+     /*
+        0)Weather main
+        1)Weather descprition
+        2)Min Tempature (f)
+        3)Max Tempature (f)
+        4)humidity percentage
+        5)Pressure (hPa)
+        6)Wind speed (m/s)
+        7)Wind direction (degrees)
+        8)Current Temp (f)
+
+        I have set location update time to 15mins and Distance to 10km
+     */
+
+     // Response for Request
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,900000,10000,locationListener);
+            }
+        }
+    }
+
     public class DownloadTask extends AsyncTask<String,Void,String> {
         @Override
         protected String doInBackground(String...urls) {
@@ -39,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
                 data = reader.read();
                 while (data != -1){
                     char c = (char) data;
-                    jsonString+=c;
+                    jsonString += c;
                     data = reader.read();
                 }
                 return jsonString;
@@ -56,25 +101,46 @@ public class MainActivity extends AppCompatActivity {
                 JSONObject jsonObject = new JSONObject(s);
                 String weatherString = jsonObject.getString("weather");
                 JSONArray jsonArray = new JSONArray(weatherString);
-                jsonObject = jsonArray.getJSONObject(0);
-                curr_weather = jsonObject.getString("description");
+                JSONObject mainObject = new JSONObject(jsonObject.get("main").toString());
+                JSONObject weatherObject = new JSONObject(jsonArray.get(0).toString());
+                JSONObject windObject = new JSONObject(jsonObject.get("wind").toString());
+
+                showDetails[0] = weatherObject.getString("main");
+                showDetails[1] = weatherObject.getString("description");
+                showDetails[2] = mainObject.getString("temp_min");
+                showDetails[3] = mainObject.getString("temp_max");
+                showDetails[4] = mainObject.getString("humidity");
+                showDetails[5] = mainObject.getString("pressure");
+                showDetails[6] = windObject.getString("speed");
+                showDetails[7] = windObject.getString("deg");
+                showDetails[8] = mainObject.getString("temp");
+                placeName = jsonObject.getString("name");
+                /*for(int i=0;i<9;i++){
+                    Log.i("info",showDetails[i]);
+                }*/
+                placeWeatherInfo();
             }
             catch (Exception e){
                 e.printStackTrace();
             }
         }
     }
-
-    public void getWeather(View view){
-        String cityName = ((TextView)findViewById(R.id.inputView)).getText().toString();
-
-        // API OpenWeather
-        DownloadTask task = new DownloadTask();
+    public void placeWeatherInfo(){
+        ((TextView)findViewById(R.id.tempView)).setText(showDetails[8]);
+        ((TextView)findViewById(R.id.weatherTitle)).setText(showDetails[0]);
+        ((TextView)findViewById(R.id.locationView)).setText(placeName);
+    }
+    public void getWeather(Location location){
+        if(location == null){
+            return;
+        }
         String jsonOutput = "";
         try {
-            jsonOutput = task.execute("http://api.openweathermap.org/data/2.5/weather?q=" +cityName+ "&appid=938bb3a45ec1e77e05c50788aeaaf94f").get();
+            // API OpenWeatherMap
+            DownloadTask task = new DownloadTask();
+            jsonOutput = task.execute("http://api.openweathermap.org/data/2.5/weather?lat="+location.getLatitude()+"&lon="+location.getLongitude()+"&appid=938bb3a45ec1e77e05c50788aeaaf94f").get();
             task.onPostExecute(jsonOutput);
-            ((TextView)findViewById(R.id.weatherView)).setText(curr_weather);
+            task.cancel(true);
         }
         catch (Exception e){
             e.printStackTrace();
@@ -82,25 +148,71 @@ public class MainActivity extends AppCompatActivity {
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Get Hour
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH");
+        final int currentTime = Integer.parseInt(simpleDateFormat.format(Calendar.getInstance().getTime()));
+
+        // Only PortraitMode
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ((ConstraintLayout)findViewById(R.id.weatherLayout)).setVisibility(View.INVISIBLE);
+
+        // Hiding Weather Layout at Startup
+        final ConstraintLayout weatherLayout = ((ConstraintLayout)findViewById(R.id.weatherLayout));
+        weatherLayout.setVisibility(View.INVISIBLE);
+
         final TextView introText = ((TextView)findViewById(R.id.introText));
         ((ImageView)findViewById(R.id.introImage)).setVisibility(View.VISIBLE);
         introText.setVisibility(View.VISIBLE);
         introText.setX(-800);
         introText.animate().translationX(5).setDuration(1000);
 
-        //Intro starts
+        // Location Service
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                getWeather(location);
+            }
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+        //Request for Location
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+        }else{
+            getWeather(locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER));
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,900000,10000,locationListener);
+        }
+        // Intro starts
         new CountDownTimer(2000,2000){
             @Override
             public void onTick(long rem){
-             }
+            }
             @Override
             public void onFinish() {
                 ((ImageView)findViewById(R.id.introImage)).setVisibility(View.INVISIBLE);
                 introText.setVisibility(View.INVISIBLE);
-                ((ConstraintLayout)findViewById(R.id.weatherLayout)).setVisibility(View.VISIBLE);
+                weatherLayout.setVisibility(View.VISIBLE);
+                if(6<=currentTime && 17>=currentTime){
+                    weatherLayout.setBackground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.daygradient));
+                }else if(18<=currentTime && 19>=currentTime){
+                    weatherLayout.setBackground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.sunset_gradient));
+                }else
+                    weatherLayout.setBackground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.night_gradient));
             }
         }.start();
     }
